493c564dbaa5ac1d3c1715ad3a8bc005aa310752
John Hunter <hunterjw.usa@gmail.com>
2024-11-18 20:58:35 -0600

    Add automatic light/dark theme

---
+++ src/BlogDotNet.BlazorWebApp/wwwroot/theme.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener('DOMContentLoaded', (e) => {
+
+    // update on initial page load
+    updateTheme();
+
+    // then update on partial page reloads
+    Blazor.addEventListener('enhancedload', () => {
+        updateTheme();
+    });
+})
+
+function updateTheme() {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        // dark mode
+        document.documentElement.setAttribute('data-bs-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-bs-theme', 'light');
+    }
+}
